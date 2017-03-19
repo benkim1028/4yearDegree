2101b415acc9affcb319e3520ee78c4308cb481a
JimFung <jfung92@gmail.com>
2017-03-18 21:16:22 -0700

    removed some old code

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-// import './App.css';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
+      <div className="App">
+        <h1>4 Year Degree</h1>
         {this.props.children}
       </div>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
+
 import List from './List'
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <h2>Testing</h2>
         <List />
       </div>
     )
