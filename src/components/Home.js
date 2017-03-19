e96970f950eb437783d05be3fa9c782183599cfe
JimFung <jfung92@gmail.com>
2017-03-18 22:24:43 -0700

    styled buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import { Button } from 'semantic-ui-react'
 
-import FacultyDropDown from './FacultyDropdown.js'
 import './Home.css'
+import FacultyDropDown from './FacultyDropdown.js'
 
 class Home extends Component {
   render() {
     return (
       <div className="DropDownContainer">
         <FacultyDropDown />
+        <div className="ButtonGroups">
+          <Button content='Back' icon='left arrow' labelPosition='left' />
+          <Button content='Next' icon='right arrow' labelPosition='right' />
+        </div>
       </div>
     )
   }
