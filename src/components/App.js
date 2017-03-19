845232c787b07817e50106a1a87b1dcc6f7ed19a
SeeratSek <seeratsek@gmail.com>
2017-03-18 21:36:45 -0700

    banner added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import './Banner.css'
 
 class App extends Component {
+
+	
   render() {
     return (
       <div className="App">
-        <h1>4 Year Degree</h1>
+      	<div className="Banner"> 4 Year Degree</div>
+        
         {this.props.children}
       </div>
     );
