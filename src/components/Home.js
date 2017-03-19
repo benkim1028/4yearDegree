b645f084ef826dca5b8c43a8eb38c5b783e06073
JimFung <jfung92@gmail.com>
2017-03-18 22:28:30 -0700

    fixed some styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,16 @@ import { Button } from 'semantic-ui-react'
 import './Home.css'
 import FacultyDropDown from './FacultyDropdown.js'
 
+// If you want a button that goes back it'll look like this
+// <Button className="NextButton" content='Back' icon='left arrow' labelPosition='left' />
+
 class Home extends Component {
   render() {
     return (
       <div className="DropDownContainer">
         <FacultyDropDown />
         <div className="ButtonGroups">
-          <Button content='Back' icon='left arrow' labelPosition='left' />
-          <Button content='Next' icon='right arrow' labelPosition='right' />
+          <Button className="NextButton" content='Next' icon='right arrow' labelPosition='right' />
         </div>
       </div>
     )
