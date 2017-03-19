8effe848e6fc92ea72187bb01de0ebfe56d8300c
SeeratSek <seeratsek@gmail.com>
2017-03-19 07:56:53 -0700

    change accordion styles

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import Home from './components/Home'
 import YearCourses from './components/YearCourses'
 import YearCoursesPage from './components/YearCoursesPage'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 // Add the reducer to your store on the `routing` key
 
 // Create an enhanced history that syncs navigation events with the store
